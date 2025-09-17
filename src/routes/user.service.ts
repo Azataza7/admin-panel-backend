a3eb0d86f0158953209d5b93d57d2b4353796093
Azat Kulbaev <kulbaevaazat@gmail.com>
2025-09-18 01:05:11 +0600

    delete method

--- src/routes/user.service.ts
+++ src/routes/user.service.ts
@@ -114,4 +114,25 @@ UserServiceRoute.put(
   }
 );
 
+UserServiceRoute.delete(
+    "/deleteUserData/:id",
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const userId = req.params.id;
+
+            const user = await User.findByPk(userId);
+            if (!user) {
+                return res.status(404).json({ error: "User not found" });
+            }
+
+            await user.destroy();
+
+            return res.status(200).json({ message: "User deleted successfully" });
+        } catch (error) {
+            console.error("ERROR -------", error);
+            next(error);
+        }
+    }
+);
+
 export default UserServiceRoute;
